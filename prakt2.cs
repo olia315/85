de5130e1013bb3b1193f4a17bfe19c43176851db
olia215 <dacenkoola3@gmail.com>
2024-11-03 15:58:38 +0200

    add prakt2.cs

---
+++ prakt2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //Задание 1.Написать метод, «рисующий» на экране горизонтальную линию из 60 символов «*».
+        //    //for(int i=0; i<60; i++)
+        //    //{
+        //    //    Console.Write("*");
+        //    //}
+
+        //    //Задание 2.Написать метод, «рисующий» прямоугольный треугольник указанного вида
+        //    //for ( int i = 1; i <= 9; i++)
+        //    //{
+        //    //    for ( int j = 1; j <= i; j++)
+        //    //    {
+        //    //        Console.Write('*');
+        //    //    }
+        //    //    Console.WriteLine(" ");
+        //    //}
+        //    //Console.ReadKey();
+
+        //    
+        //}
+        //Задание 3.Вычислить значение выражения, определив и использовав дополнительный метод:
+        //static double Func()
+        //{
+        //    double x;
+        //    x = (Math.Sqrt(5) + 5) / 2 + (Math.Sqrt(12) + 12) / 2 + (Math.Sqrt(19) + 19) / 2;
+        //    return x;
+        //}
+        //static void Main()
+        //{
+        //    Console.WriteLine(Func());
+        //}
+
+        //Задание 4. Нахождение максимальной величины из двух целых переменных a, b.
+        //static int Max(int a, int b)
+        //{
+        //    return (a > b) ? a : b;
+        //}
+        //static void Main()
+        //{
+        //    Console.Write("Введите а=");
+        //    int a = int.Parse(Console.ReadLine());
+        //    Console.Write("Введите b=");
+        //    int b = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Макимальное число: "+Max(a,b));
+        //}
+
+
+
+    }
+}
+
